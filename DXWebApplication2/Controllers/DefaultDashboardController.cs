1311bbdc34651878ec5398a05ff69d0e4b123f0a
AHMET SUAT DIRAV <adirav19@posta.pau.edu.tr>
2023-04-19 09:55:41 +0300

    Proje dosyası ekle.

---
+++ DXWebApplication2/Controllers/DefaultDashboardController.cs
@@ -0,0 +1,46 @@
+﻿﻿using DevExpress.DashboardAspNetCore;
+using DevExpress.DashboardWeb;
+using DXWebApplication2.Models;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+
+namespace DXWebApplication2.Controllers {
+    public class DefaultDashboardController : DashboardController {
+        public DefaultDashboardController(DashboardConfigurator configurator, IDataProtectionProvider? dataProtectionProvider = null)
+            : base(configurator, dataProtectionProvider) {
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        /*[HttpPost]
+
+        public async Task<IActionResult> Index(Giris p)
+        {
+            Context c = new Context();
+            var datavalue = c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail
+            && x.WriterPass == p.WriterPass);
+            if (datavalue != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,p.WriterMail)
+                };
+                var useridentity = new ClaimsIdentity(claims, "a");
+                ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
+                await HttpContext.SignInAsync(principal);
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                return View();
+            }
+        }*/
+    }
+}
+
+
+
---
+++ DXWebApplication2/Models/Giris.cs
@@ -0,0 +1,31 @@
+﻿namespace DXWebApplication2.Models
+{
+    public class Giris
+    {
+
+
+
+        static Random rnd1 = new Random();
+
+        public string? userid { get; set; }
+        public string? password { get; set; }
+
+
+        public static List<Giris> CreateData()
+        {
+            List<Giris> data = new List<Giris>();
+            data.Add(new Giris { userid = "ahmetsuatdirav4@gmail.com", password = "pass" });
+            return data;
+        }
+
+        static DateTime GenerateOrderDate()
+        {
+            int startYear = DateTime.Today.Year - 3;
+            int endYear = DateTime.Today.Year;
+            return new DateTime(rnd1.Next(startYear, endYear), rnd1.Next(1, 13), rnd1.Next(1, 29));
+        }
+
+
+
+    }
+}
